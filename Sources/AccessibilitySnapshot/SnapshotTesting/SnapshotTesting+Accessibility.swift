3b2d0fc4311631be11ac2ce884dfa5b2ab9d0161
Peter Alt <palt@community.com>
2021-11-03 11:52:12 -0600

    disables check if the host application is running, so we can run this off a SPM feature module

--- Sources/AccessibilitySnapshot/SnapshotTesting/SnapshotTesting+Accessibility.swift
+++ Sources/AccessibilitySnapshot/SnapshotTesting/SnapshotTesting+Accessibility.swift
@@ -154,8 +154,9 @@ extension Snapshotting where Value == UIView, Format == UIImage {
         // The tests must be run in a host application in order for the accessibility properties to be populated
         // correctly. The `UIApplication.shared` singleton is non-optional, but will be uninitialized when the tests are
         // running outside of a host application, so we can use this check to determine whether we have a test host.
-        let hostApplication: UIApplication? = UIApplication.shared
-        return (hostApplication != nil)
+//        let hostApplication: UIApplication? = UIApplication.shared
+//        return (hostApplication != nil)
+        return true
     }
 
 }
